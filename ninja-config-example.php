4d5b12c404d76756f26a00944f497eaf7c880f7a
DalatCoder <hieuntctk42@gmail.com>
2021-11-01 12:52:47 +0700

    Init Commit

---
+++ ninja-config-example.php
@@ -0,0 +1,13 @@
+<?php
+
+$ninja_global_configs = [
+    'shop_name' => 'Shop Name',
+    'routes' => true,
+    'auth' => false,
+    'permission' => false,
+    'db_name' => 'db_name',
+    'db_host' => 'localhost',
+    'db_charset' => 'utf8',
+    'db_username' => 'root',
+    'db_password' => ''
+];
